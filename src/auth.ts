b84117f31076d1eaa257fed2fcb017e05325b539
Mostafiz <mostafiz4372@gmail.com>
2026-04-04 12:11:36 +0600

    google provider added

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
 import NextAuth from "next-auth"
 import Credentials from "next-auth/providers/credentials"
 import { User } from "./server/module/models/user/user.model"
 import connectDb from "./server/lib/db"
 import bcrypt from "bcryptjs"
+import Google from "next-auth/providers/google"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [
@@ -34,7 +36,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 
                 const isMatch = await bcrypt.compare(credentials.password as string, user.password)
 
-                if(!isMatch){
+                if (!isMatch) {
                     throw Error("incorrect Password")
                 }
 
@@ -45,6 +47,63 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
                     role: user.role
                 }
             },
+        }),
+
+        Google ({
+            clientId: process.env.AUTH_GOOGLE_ID,
+            clientSecret: process.env.AUTH_GOOGLE_SECRET
         })
     ],
+
+    callbacks: {
+        async signIn({user, account}){
+            if(account?.provider === "google"){
+                await connectDb()
+                const dbUser = await User.findOne({email: user.email})
+                if(!dbUser){
+                    await User.create({
+                        name: user.name,
+                        email: user.email
+                    })
+                }
+
+                user.id = dbUser._id
+                user.role = dbUser.role
+            }
+
+            return true
+        },
+
+        async jwt({ token, user }) {
+            token.name = user.name,
+                token.id = user.id,
+                token.email = user.email,
+                token.role = user.role
+
+            return token
+        },
+
+        async session({ token, session }) {
+            if (session.user) {
+                session.user.name = token.name,
+                session.user.id = token.id as string,
+                session.user.email = token.email as string,
+                session.user.role = token.role as string
+            }
+
+            return session
+        }
+    },
+
+    pages: {
+        signIn: "/signin",
+        error: "/signin"
+    },
+
+    session: {
+        strategy: "jwt",
+        maxAge: 10*24*60*60
+    },
+
+    secret: process.env.AUTH_SECRET
 })
---
+++ src/types.d.ts
@@ -0,0 +1,8 @@
+
+declare module "next-auth" {
+    interface User{
+        role: string
+    }
+}
+
+export {}
